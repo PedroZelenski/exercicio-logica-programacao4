7ae5b74ccaf4b4dceaaa1ab8fe737921e854c7d6
Zelenski <zelenski.contato@gmail.com>
2020-07-29 15:19:16 -0300

    first commit

---
+++ BhaskaraService.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace exercicio_logica_programacao4
+{
+    public class BhaskaraService
+    {
+        public double NumeroA { get; set; }
+        public double NumeroB { get; set; }
+        public double NumeroC { get; set; }
+
+        public BhaskaraService()
+        {
+            Console.WriteLine("Equacao de segundo grau: Ax + Bx + C = 0");
+
+            Console.WriteLine("----------");
+
+            Console.WriteLine("Digite o valor de A: ");
+            NumeroA = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Digite o valor de B: ");
+            NumeroB = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Digite o valor de C: ");
+            NumeroC = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("----------");
+        }
+
+        public void CalcularBhaskara()
+        {
+            double delta = (NumeroB * NumeroB) - 4 * NumeroA * NumeroC;
+            double raiz = Math.Sqrt(delta);
+            var x1 = ((-(NumeroB)) + raiz) / (2 * NumeroA);
+            var x2 = ((-(NumeroB)) - raiz) / (2 * NumeroA);
+
+            Console.WriteLine($"Valor de x1: {x1}");
+            Console.WriteLine($"Valor de x2: {x2}");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace exercicio_logica_programacao4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BhaskaraService bhaskaraService = new BhaskaraService();
+            bhaskaraService.CalcularBhaskara();
+        }
+    }
+}
